7fcd21af11668a3f5751f3cb1e83610f7851af08
Denny <hong961127@gmail.com>
2021-04-04 18:35:44 -0700

    Add stream solutions

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const fs = require("fs");
 function doOnRequest(request, response) {
 	if (request.method === "GET" && request.url === "/") {
 		// read the index.html file and send it back to the client
-		// fs.createReadStream()
-		const file = fs.readFileSync("index.html");
-		response.end(file);
+		fs.readFile("index.html", (err, file) => {
+			response.end(file);
+		});
 	} else if (request.method === "GET" && request.url === "/style.css") {
-		const cssFile = fs.readFileSync("style.css");
-		response.end(cssFile);
+		fs.readFile("style.css", (err, cssFile) => {
+			response.end(cssFile);
+		});
 	} else if (request.method === "POST" && request.url === "/sayHi") {
-		fs.appendFileSync("hi_log.txt", "Somebody said hi.\n", { encoding: "utf-8" });
-		response.end("hi back to you!");
+		fs.appendFile("hi_log.txt", "Somebody said hi.\n", (err) => {
+			response.end("hi back to you!");
+		});
 	} else if (request.method === "POST" && request.url === "/greeting") {
 		// accumulate the request body in a series of chunks
 		let reqBody = [];
@@ -23,15 +25,16 @@ function doOnRequest(request, response) {
 			})
 			.on("end", () => {
 				reqBody = Buffer.concat(reqBody).toString();
-				fs.appendFileSync("hi_log.txt", `${reqBody}\n`, { encoding: "utf-8" });
 
-				if (reqBody === "hello") {
-					response.end("hello there!");
-				}
+				fs.appendFile("hi_log.txt", `${reqBody}\n`, (err) => {
+					if (reqBody === "hello") {
+						response.end("hello there!");
+					}
 
-				if (reqBody === "what's up") {
-					response.end("the sky");
-				}
+					if (reqBody === "what's up") {
+						response.end("the sky");
+					}
+				});
 			});
 	} else if (request.method === "PUT" && request.url === "/update") {
 		let reqBody = [];
@@ -42,12 +45,14 @@ function doOnRequest(request, response) {
 			})
 			.on("end", () => {
 				reqBody = Buffer.concat(reqBody).toString();
-				fs.writeFileSync("hi_log.txt", `${reqBody}\n`, { encoding: "utf-8" });
-				response.end("Updated!");
+				fs.writeFile("hi_log.txt", `${reqBody}\n`, (err) => {
+					response.end("Updated!");
+				});
 			});
 	} else if (request.method === "DELETE" && request.url === "/delete") {
-		fs.unlinkSync("hi_log.txt");
-		response.end("Deleted!");
+		fs.unlink("hi_log.txt", (err) => {
+			response.end("Deleted!");
+		});
 	} else {
 		// Handle 404 error: page not found
 		response.statusCode = 404;
--- streams/streams.js
+++ streams/streams.js
@@ -1,15 +1,32 @@
-const fs = require('fs');
-const through = require('through2');
+const fs = require("fs");
+const through2 = require("through2");
 
-/* Create a read stream here
-const readPoemStream = ???
-*/
+// Create a read stream here
+const readPoemStream = fs.createReadStream("./on-joy-and-sorrow-emoji.txt");
 
-/* Create a write stream here
-const writePoemStream =
-*/
+// readPoemStream.on("data", (chunk) => {
+// 	console.log(chunk.toString().replace(/:\)/gi, "joy").replace(/:\(/gi, "sorrow"));
+// });
+
+// Create a write stream here
+const writePoemStream = fs.createWriteStream("./on-joy-and-sorrow-fixed.txt");
+
+// writePoemStream.on("pipe", (src) => {
+// 	console.log({ src });
+// });
+
+readPoemStream.pipe(writePoemStream);
 
 /* EXTENSION: Create a transform stream (modify the read stream before piping to write stream)
 const transformStream = ???
 readPoemStream.pipe(transformStream).pipe(writePoemStream)
 */
+
+const transformStream = through2(function (bufferChunk, encoding, next) {
+	let text = bufferChunk.toString();
+	text = text.replace(/:\)/gi, "joy").replace(/:\(/gi, "sorrow");
+	this.push(text);
+	next();
+});
+
+readPoemStream.pipe(transformStream).pipe(writePoemStream);
